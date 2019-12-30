d35e1939285a1239b5dcd64ec80bdd65573e5d0a
JoaoAlan <professorjoaomoreira@gmail.com>
2019-12-30 03:18:53 -0300

    site3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# REACT_3_SITE
+# React Series
+Este pacote contém os arquivos básicos para iniciar as vídeo aulas
+de React do [Canal V++](https://youtube.com/user/VPlusPlus).
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Contador from './Contador';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <div>
+        <Contador />
+    </div>
+
+  );
+    
+  }
+}
